9e81ca99256355d149a8cedd686b164cc36ceaee
Khushi Naresh <khushinaresh007@gmail.com>
2025-12-01 21:42:14 +0530

    bruh

---
+++ app/components/ui/dt-et.tsx
@@ -0,0 +1,16 @@
+export default function DtEt() {
+    return (
+        <div className="p-6 bg-black text-white rounded-xl
+        border border-whitw/20">
+            <h1 className="text-2xl font-bold"> Drive Train & Engine and Tractive System Dashboard</h1>
+            <p className="text-neutral-300-mt-2">
+                This dashboard provides insights into the performance and efficiency of the drive train, engine, and tractive system components. 
+            </p>
+         </div>
+    );
+}
+
+                
+            
+
+  
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import DtEt from "./components/ui/dt-et";
+export default function Home() {
+  return (
+    <main className="min-h-screen p-10 bg-black text-white">
+      <DtEt />
+    </main>
+  );
+}
