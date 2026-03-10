27f5c2b6b39e261309bf4af1d3305fb06200ed76
1Aditya7 <aditya17parthiban@gmail.com>
2026-03-10 21:04:13 +0530

    changed button text to 'Click Me'

--- cost-presentation-dashboard/src/app/page.tsx
+++ cost-presentation-dashboard/src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button variant="outline">Button</Button>
+      <Button variant="outline">Click Me</Button>
     </div>
   );
 }
